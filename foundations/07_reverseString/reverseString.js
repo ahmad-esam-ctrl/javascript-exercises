b98431e8037583a0945cb799d59ce8040ff02394
Ahmed <ahmedesamdeveloper@gmail.com>
2026-08-13 03:19:41 +0300

    I have finished reverseString task

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (word) {
+    let finalText = "";
+    let len = word.length - 1;
+    for (let i = len; i >= 0; i--) {
+        finalText += word[i]
+    }
+    return finalText
 };
 
 // Do not edit below this line

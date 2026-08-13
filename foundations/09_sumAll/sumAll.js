e8558666dcf126ca2e194ae59f8e1a43a0dba85b
Ahmed <ahmedesamdeveloper@gmail.com>
2026-08-14 02:58:08 +0300

    I have finished sumAll task

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+    let numbers = [];
+    let total = 0;
+    if (Number.isInteger(num1) == false || Number.isInteger(num2) == false || num1 < 0 || num2 < 0) {
+        return "ERROR";
+    } else {
 
+        if (num1 < num2) {
+            for (let i = num1; i <= num2; i++) {
+                numbers.push(i);
+            };
+        } else if (num1 > num2) {
+            for (let i = num2; i <= num1; i++) {
+                numbers.push(i);
+            };
+        };
+        for (const num of numbers) {
+            total += num;
+        };
+    };
+    return total;
 };
 
 // Do not edit below this line

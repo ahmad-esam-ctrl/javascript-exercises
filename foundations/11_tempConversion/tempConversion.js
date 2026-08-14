5f85b42e64913182fc97c978524f0b3488918c28
Ahmed <ahmedesamdeveloper@gmail.com>
2026-08-14 04:28:09 +0300

    I have finished tempConversion task

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (degree) {
+  let convertedDegree = (degree - 32) * 5 / 9;
+  if (convertedDegree % 1 != 0) {
+    let result = convertedDegree.toFixed(1)
+    return Number(result);
+  };
+  return Number(convertedDegree);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (degree) {
+  let convertedDegree = (degree * 9 / 5) + 32;
+  if (convertedDegree % 1 != 0) {
+    let result = convertedDegree.toFixed(1)
+    return Number(result);
+  };
+  return Number(convertedDegree);
 };
 
 // Do not edit below this line
